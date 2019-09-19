7ea458691e8d0c351fb67a8ab6570747ce9f6566
Ilya Dudkin <ilya_dudkin@epam.com>
2019-09-19 13:08:25 +0300

    2nd commit

--- src/main/java/com/ontotext/trree/plugin/CdcPlugin.java
+++ src/main/java/com/ontotext/trree/plugin/CdcPlugin.java
@@ -1,82 +1,102 @@
 package com.ontotext.trree.plugin;
 
 import com.ontotext.trree.sdk.*;
-import com.ontotext.trree.sdk.Statements.Listener;
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.util.iterators.SingletonIterator;
 
+import java.util.Iterator;
 
-public class CdcPlugin extends PluginBase implements UpdateInterpreter {
+public class CdcPlugin extends PluginBase implements PatternInterpreter, Preprocessor, Postprocessor, Configurable {
 
-  private static final String PREFIX = "http://example.com/";
+  private static final String PREDICATE_PARAM = "predicate-uri"; // parameter name
 
-  // IDs of the entities in the entity pool
-  private long ids;
-
+  // Service interface methods
   @Override
   public String getName() {
-    return "CDC-Plugin";
+    return "example";
   }
 
   // Plugin interface methods
   @Override
   public void initialize(InitReason reason) {
-    IRI iri = SimpleValueFactory.getInstance().createIRI(PREFIX);
-    ids = getEntities().put(iri, Entities.Scope.SYSTEM);
     getLogger().info("CDC plugin initialized!");
   }
 
-  // UpdateInterpreter interface methods
   @Override
-  public long[] getPredicatesToListenFor() {
-    return new long[] {ids};
+  public StatementIterator interpret(long subject, long predicate, long object, long context,
+      Statements statements, Entities entities, RequestContext requestContext) {
+    return null;
   }
 
   @Override
-  public boolean interpretUpdate(long subject, long predicate, long object, long context, boolean isAddition, boolean isExplicit, Statements statements, Entities entities) {
+  public double estimate(long subject, long predicate, long object, long context,
+      Statements statements, Entities entities, RequestContext requestContext) {
+    // We always return a single statement
+    return 1;
+  }
 
-    Listener listener = new Listener() {
+  // Preprocessor interface methods
+  @Override
+  public RequestContext preprocess(Request request) {
+    if (request instanceof UpdateRequest) {
+      UpdateRequest updateRequest = (UpdateRequest) request;
+      getLogger().info(updateRequest.getUpdateExpr().toString());
+    }
+    if (request instanceof QueryRequest) {
+      QueryRequest queryRequest = (QueryRequest) request;
+      getLogger().info(queryRequest.getTupleExpr().toString());
+    }
+    return null;
+  }
 
-      @Override
-      public boolean statementAdded(long l, long l1, long l2, long l3, long l4, boolean b) {
-        String res = "Added: " + subject + " " + predicate + " " + object;
-        System.out.println(res);
-        getLogger().info(res);
-        return false;
-      }
-
-      @Override
-      public boolean statementRemoved(long l, long l1, long l2, long l3, long l4, boolean b) {
-        String res = "Removed: " + subject + " " + predicate + " " + object;
-        System.out.println(res);
-        getLogger().info(res);
-        return false;
-      }
-
-      @Override
-      public void transactionStarted(long tid) {
-        String res = "Started transaction " + tid;
-        System.out.println("Started transaction " + tid);
-        getLogger().info(res);
-      }
-
-      @Override
-      public void transactionCompleted(long tid) {
-        String res = "Finished transaction " + tid;
-        System.out.println(res);
-        getLogger().info(res);
-      }
-
-      @Override
-      public void transactionAborted(long tid) {
-        String res = "Aborted transaction " + tid;
-        System.out.println(res);
-        getLogger().info(res);
-      }
-    };
-
-    statements.addListener(listener);
+  // Postprocessor interface methods
+  @Override
+  public boolean shouldPostprocess(RequestContext requestContext) {
     return false;
   }
 
+  @Override
+  public BindingSet postprocess(BindingSet bindingSet, RequestContext requestContext) {
+    return null;
+  }
+
+  @Override
+  public Iterator<BindingSet> flush(RequestContext requestContext) {
+    // return the binding set we have created in the preprocess phase
+    BindingSet result = ((Context) requestContext).getResult();
+    return new SingletonIterator<>(result);
+  }
+
+  // Configurable interface methods
+  @Override
+  public String[] getParameters() {
+    // GraphDB should expect our parameter
+    return new String[] { PREDICATE_PARAM };
+  }
+
+  /**
+   * Context where we can store data during the processing phase
+   */
+  private static class Context implements RequestContext {
+    private Request theRequest;
+    private BindingSet theResult;
+
+    public Context(BindingSet result) {
+      theResult = result;
+    }
+
+    @Override
+    public Request getRequest() {
+      return theRequest;
+    }
+
+    @Override
+    public void setRequest(Request request) {
+      theRequest = request;
+    }
+
+    public BindingSet getResult() {
+      return theResult;
+    }
+  }
 }
